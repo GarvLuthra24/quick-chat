ba3484c13d86fd29266c6ddd33b899f3fb8f5ee0
Garv <99045108+GarvLuthra24@users.noreply.github.com>
2024-03-24 03:08:05 +0530

    vercel fixes

--- server/index.js
+++ server/index.js
@@ -11,6 +11,11 @@ const app = express()
 require("dotenv").config()
 
 app.use(cors({credentials: true}))
+app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.json())
 app.use('/api/message', messageRoute)
 app.use('/api/auth',userRoutes)
