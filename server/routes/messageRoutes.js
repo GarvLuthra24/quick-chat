bf6d716f07b3924827c2a397d8d2896ca4b34462
Garv <99045108+GarvLuthra24@users.noreply.github.com>
2024-03-24 04:09:46 +0530

    asfasfasfasfasfdasdf

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const messageController = require('../controller/messageController')
 
-router.post('/hi' , (req , res , next )=> {return res.json({message:"hi"})})
+router.get('/hi' , (req , res , next )=> {return res.json({message:"hi"})})
 
 router.post("/add-message", messageController.addMessage)
 router.post("/get-message", messageController.getMessages)
